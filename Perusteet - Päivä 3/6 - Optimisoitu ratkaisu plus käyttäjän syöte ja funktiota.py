3d88bf5cb2659564558c44f434e53866435265cb
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-10-27 10:18:35 +0200

    20251027_101835

--- Perusteet - Päivä 3/6 - Optimisoitu ratkaisu plus käyttäjän syöte ja funktiota.py
+++ Perusteet - Päivä 3/6 - Optimisoitu ratkaisu plus käyttäjän syöte ja funktiota.py
@@ -4,21 +4,7 @@ MOVES = ['kivi', 'sakset', 'paperi']
 INITIALS = ['k', 's', 'p']
 ANSWERS = INITIALS + MOVES
 
-def get_move(name: str) -> int:
-    """
-    Get the player move and returns it in integer form
-
-    This function will ask the user to choose between 'Kivi', 'Sakset' and 'Paperi'.
-    The whole word or initials are accepted (case insensitive).
-    It will keep asking the user until it gets a valid answer. It can not be cancelled.
-
-    Parameters:
-    - name (str): Name of the player that will be presented in the question
-
-    Returns:
-    An integer that represents the index in the options.
-    """
-
+def get_move(name):
     answer = ''
     while answer not in ANSWERS:
         answer = input(f'{name}: [K]ivi, [S]akset tai [P]aperi? ').lower()
@@ -27,27 +13,17 @@ def get_move(name: str) -> int:
     else:
         return MOVES.index(answer)
 
-def play(player_1: int, player_2: int) -> str:
-    """
-    Calculate the winner according to the rules of the game and return a string declaring the winner.
-
-    Parameters:
-    - player_1 (int): The first player move. It's an index of the list of possible moves.
-    - player_2 (int): The second player move. It's an index of the list of possible moves.
-
-    Returns:
-    A string containing a message declaring the winner or that the game was a draw.
-    """
+def play(player_1, player_2):
     if player_1 == player_2:
         return 'Tasapeli!'
     elif (player_1 + 1) % 3 == player_2:
-        return 'Pelaaja 1 voittaa'
+        return 'Player 1 wins'
     else:
-        return 'Pelaaja 2 voittaa'
+        return 'Player 2 wins'
 
 
-player_1 = get_move('Pelaaja 1')
-player_2 = get_move('Pelaaja 2')
+player_1 = get_move('Player 1')
+player_2 = get_move('Player 2')
 
 print(play(player_1, player_2))
 
