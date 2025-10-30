e911fffabf0187172637dcd6843c86c14bd00181
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-10-30 11:09:52 +0200

    20251030_110952

--- Perusteet - Päivä 6/hirsipuu.py
+++ Perusteet - Päivä 6/hirsipuu.py
@@ -1,33 +1,41 @@
 hidden_word = 'hirsipuu'
 resolved_word = ['_'] * len(hidden_word)
-VALID_LETTERS='abcdefghijklmnopqrstuvwxyzäö'
+VALID_LETTERS = 'abcdefghijklmnopqrstuvwxyzäö'
 
 
 def ask_user(question: str) -> str:
     while True:
         answer = input(question).lower()
-        if answer == '':
-            continue
-        else:
-            for letter in answer:
-                if letter not in VALID_LETTERS:
-                    break
-            else:
-                return answer
+        match answer:
+            case '':
+                continue
+            case '0':
+                return '0'
+            case _:
+                for letter in answer:
+                    if letter not in VALID_LETTERS:
+                        break
+                else:
+                    return answer
 
-while resolved_word.count('_') >= 1:
+while True:
     print(f"Arvattava sana on {''.join(resolved_word)}")
-    user_answer = ask_user('Syötä kirjain tai sana (A-Za-zÄäÖö)\n? ')
+    user_answer = ask_user('Syötä kirjain tai sana (A-Za-zÄäÖö). 0 -> lopettaa peli\n? ')
+    match user_answer:
+        case '0':
+            print('Kiitos pelaamisesta')
+            break
+        case _ if len(user_answer) == 1:
+            for index, word_letter in enumerate(hidden_word):
+                if user_answer == word_letter:
+                    resolved_word[index] = user_answer
+        case _ if user_answer == hidden_word:
+           resolved_word = list(hidden_word)
     
-    if len(user_answer) == 1:
-        for index, word_letter in enumerate(hidden_word):
-            if user_answer == word_letter:
-                resolved_word[index] = user_answer
-    else:
-        if user_answer == hidden_word:
-           break 
+    if resolved_word.count('_') == 0:
+           print(f'Voitit. Sana oli {hidden_word}')
+           break
 
 
-print(f'Voitit. Sana oli {hidden_word}')
 
 
