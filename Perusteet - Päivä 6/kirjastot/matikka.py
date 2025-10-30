fe52c61126f5c54f000ebe632e6f0f09144c85ad
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-10-30 14:10:03 +0200

    20251030_141003

---
+++ Perusteet - Päivä 6/kirjastot/matikka.py
@@ -0,0 +1,18 @@
+import math
+
+a = 5
+
+def summa(a, b):
+    return a + b
+
+def erotus(a, b):
+    return a - b
+
+def main():
+    pass
+
+print(f'Matikka: {__name__=}')
+
+if __name__ == '__main__':
+    print('Moi')
+    # main()
---
+++ Perusteet - Päivä 6/main_script.py
@@ -0,0 +1,29 @@
+import math
+
+# import matikka
+# print(matikka.summa(2, 2))
+# print(matikka.erotus(10, 5))
+
+
+# from matikka import summa, a
+# print(summa(2, 2))
+# print(a)
+
+
+# import matikka as mtk
+# print(mtk.summa(2, 2))
+
+
+import kirjastot.matikka as mtk
+
+print(mtk.summa(2, 2))
+print(f'Main script: {__name__}')
+
+
+# from kirjastot.matikka import (
+#     summa,
+#     erotus,
+#     a,
+# )
+
+
