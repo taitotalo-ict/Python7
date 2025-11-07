d9d81e41d58850b4da47210f2ecf49f1f3b6fc15
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-11-07 09:22:47 +0200

    20251107_092247

---
+++ Perusteet - Päivä 10/aoc_2015_2.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+SCRIPT_ROOT = Path(__file__).parent
+
+with open(SCRIPT_ROOT / 'input_2015_2.txt') as file:
+    content = [tuple(map(int, line.rstrip().split('x'))) for line in file.readlines()]
+    # content = []
+    # for line in file.readlines():
+    #     content.append(tuple(map(int, line.rstrip().split('x'))))
+
+total_gift_paper = 0
+for length, height, width in content:
+    area = 2 * length * width +  2 * width * height + 2 * height * length
+    smallest_side = min(length * width, width * height, height * length)
+    total_gift_paper += area + smallest_side
+
+print(total_gift_paper)
---
+++ Perusteet - Päivä 10/list_comp.py
@@ -0,0 +1,7 @@
+# lista = []
+# for i in range(10):
+#     if i % 2 == 0:
+#         lista.append(str(i))
+
+lista = [str(i) for i in range(10)]
+print(lista)
