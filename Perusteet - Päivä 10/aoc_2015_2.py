5f7bca1fddba761302b073f487c02558751d33d7
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-11-07 10:03:06 +0200

    20251107_100306

--- Perusteet - Päivä 10/aoc_2015_2.py
+++ Perusteet - Päivä 10/aoc_2015_2.py
@@ -9,10 +9,16 @@ with open(SCRIPT_ROOT / 'input_2015_2.txt') as file:
     # for line in file.readlines():
     #     content.append(tuple(map(int, line.rstrip().split('x'))))
 
-total_gift_paper = 0
-for length, height, width in content:
-    area = 2 * length * width +  2 * width * height + 2 * height * length
-    smallest_side = min(length * width, width * height, height * length)
-    total_gift_paper += area + smallest_side
+total_gift_paper = sum(
+    2 * length * width +  2 * width * height + 2 * height * length 
+    + min(length * width, width * height, height * length)
+    for length, height, width in content
+    )
+
+# total_gift_paper = 0
+# for length, height, width in content:
+#     area = 2 * length * width +  2 * width * height + 2 * height * length
+#     smallest_side = min(length * width, width * height, height * length)
+#     total_gift_paper += area + smallest_side
 
 print(total_gift_paper)
---
+++ Perusteet - Päivä 10/polku_harjoitus.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+size = sum(item.stat().st_size for item in Path(r'C:\Windows').glob('*') if item.is_file())
+# size = 0
+# for item in Path(r'C:\Windows').glob('*'):
+#     if item.is_file():
+#         size += item.stat().st_size
+
+print(f"{size/1024/1024:.2f} MiB")
