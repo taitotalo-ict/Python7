6416ee78a9a01036c6825a74cfd33367fb355023
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-11-11 12:53:29 +0200

    20251111_125329

--- Perusteet - Päivä 11/aoc_2024_2.py
+++ Perusteet - Päivä 11/aoc_2024_2.py
@@ -5,19 +5,13 @@ BASEDIR = Path(__file__).parent
 def is_safe(report: tuple[int, ...]) -> bool:
     diffs = [report[index+1] - report[index] for index in range(len(report)-1)]
     return all(map(lambda num: 0 < num < 4, diffs)) or \
-           all(map(lambda num: 0 > num > -4, diffs))
+           all(0 > num > -4 for num in diffs)
 
 def is_safe_removing(report: tuple[int, ...]) -> bool:
     return any(is_safe(report[:i] + report[i+1:]) for i in range(len(report)))
-    # for i in range(len(report)):
-    #     if is_safe(report[:i] + report[i+1:]):
-    #         return True
-    # return False
 
 with open(BASEDIR / 'input_2024_2.txt', 'r', encoding='utf-8') as file:
     data = [tuple(map(int, line.strip().split())) for line in file]
 
-safe = sum(is_safe(report) for report in data)
-print(safe)
-safe2 = sum(is_safe_removing(report) for report in data)
-print(safe2)
+print(sum(is_safe(report) for report in data))
+print(sum(is_safe_removing(report) for report in data))
