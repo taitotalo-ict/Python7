0f6048077ec83db173474f0dd06737f0873d55db
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-11-11 12:48:53 +0200

    20251111_124853

--- Perusteet - Päivä 11/aoc_2024_2.py
+++ Perusteet - Päivä 11/aoc_2024_2.py
@@ -2,36 +2,22 @@ from pathlib import Path
 
 BASEDIR = Path(__file__).parent
 
-def is_between_1_and_3(num: int) -> bool:
-    return 0 < num < 4
-    # return 1 <= num <= 3
-    # return (num >= 1 and num <= 3)
-    # return True if num >= 1 and num <= 3 else False
-    # if num >= 1 and num <= 3:
-    #     return True
-    # else:
-    #     return False
-
-def is_negative_between_1_and_3(num: int) -> bool:
-    return 0 > num > -4
-
 def is_safe(report: tuple[int, ...]) -> bool:
     diffs = [report[index+1] - report[index] for index in range(len(report)-1)]
     return all(map(lambda num: 0 < num < 4, diffs)) or \
            all(map(lambda num: 0 > num > -4, diffs))
 
-    # is_negative = diffs[0] < 0
-    # for diff in diffs:
-    #     if abs(diff) < 1 or abs(diff) > 3:
-    #     # if not(0 < abs(diff) < 4):
-    #         return False
-    #     if is_negative != (diff < 0):
-    #         return False
-    # return True
-
+def is_safe_removing(report: tuple[int, ...]) -> bool:
+    return any(is_safe(report[:i] + report[i+1:]) for i in range(len(report)))
+    # for i in range(len(report)):
+    #     if is_safe(report[:i] + report[i+1:]):
+    #         return True
+    # return False
 
 with open(BASEDIR / 'input_2024_2.txt', 'r', encoding='utf-8') as file:
     data = [tuple(map(int, line.strip().split())) for line in file]
 
 safe = sum(is_safe(report) for report in data)
 print(safe)
+safe2 = sum(is_safe_removing(report) for report in data)
+print(safe2)
