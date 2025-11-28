047f18a247f1993dd1ce0eb50c550cbf066dae07
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-11-28 14:09:14 +0200

    20251128_140914

---
+++ Olio4/AoC_2024_11.py
@@ -0,0 +1,18 @@
+def blink(stone: str, count: int) -> int:
+    if count == 0:
+        return 1
+    if stone == '0':
+        return blink('1', count-1) 
+    elif len(stone) % 2 == 0:
+        middle = len(stone) // 2
+        part1 = str(int(stone[:middle]))
+        part2 = str(int(stone[middle:]))
+        return blink(part1, count-1) + blink(part2, count-1)
+    else:
+        return blink(str(int(stone) * 2024), count-1)
+
+stones = '5688 62084 2 3248809 179 79 0 172169'
+print(sum(blink(stone, 25) for stone in stones.split()))
+print(sum(blink(stone, 75) for stone in stones.split()))
+
+    
