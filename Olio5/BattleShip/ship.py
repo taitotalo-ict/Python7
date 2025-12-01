0f6cba21a1216546825ff1f42369f86608a92682
Christian Finnberg <christian.finnberg@taitotalo.fi>
2025-12-01 14:05:22 +0200

    20251201_140522

--- Olio5/BattleShip/ship.py
+++ Olio5/BattleShip/ship.py
@@ -1,44 +1,49 @@
 class ShipPart:
     def __init__(self, ship: 'Ship', hidden: bool = False) -> None:
-        pass
+        self.ship = ship
+        self.hidden = hidden
+        self.hit_status = False 
 
     def is_hit(self) -> bool:
         '''Returns True if ship part has been hit/destroyed. False otherwise.'''
-        pass
+        return self.hit_status
 
     def hit(self) -> 'Ship':
         '''Ship part receives a hit. Returns the ship receiving the hit.'''
-        pass
+        self.hit_status = True
+        return self.ship
     
     def __repr__(self) -> str:
         '''Returns representation of the ship part.'''
-        pass
+        return f'ShipPart(ship={self.ship}, hidden={self.hidden}, hit_status={self.hit_status})'
     
     def __str__(self) -> str:
-        '''Returns a character representing the ship part: 'X' if hit, if not hit, '·' or '#' depending on being hidden.'''
-        pass
+        '''Returns a character representing the ship part: 'X' if hit, if not hit, '·' or '#' depending on being hidden or not respectively.'''
+        return 'X' if self.hit_status else ('·' if self.hidden else '#')
 
 
 class Ship:
     def __init__(self, size:int, hidden:bool = False) -> None:
-        pass
+        # self.size = size
+        # self.hidden = hidden
+        self.parts = [ShipPart(self, hidden) for _ in range(size)]
 
     def is_destroyed(self):
         '''Returns True if ship have been completely destroyed. False if some part has not been hit.'''
-        pass
+        return all(part.is_hit() for part in self.parts)
 
     def __getitem__(self, index: int) -> ShipPart:
         '''Returns ship part in position 'index'.'''
-        pass
+        return self.parts[index]
 
     def __len__(self) -> int:
         '''Returns length of the ship.'''
-        pass
+        return len(self.parts)
 
     def __contains__(self, part: ShipPart) -> bool:
         '''Returns True if ship contains given ship part. False otherwise.'''
-        pass
+        return part in self.parts
 
     def __repr__(self) -> str:
         '''Returns representation of the ship.'''
-        pass
+        return f'Ship(size={len(self.parts)})'
