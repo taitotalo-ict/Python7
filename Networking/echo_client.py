409eaa62f5c85e1fafece20f5d7831d26bcdecbd
Christian Finnberg <christian.finnberg@taitotalo.fi>
2026-02-19 14:51:24 +0200

    20260219_145124

--- Networking/echo_client.py
+++ Networking/echo_client.py
@@ -10,7 +10,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as connection_socket:
         message = input('Message (q -> quit): ')
         if message == 'q':
             break
-        connection_socket.sendall(message.encode())
+        elif not message:
+            continue
+        c = connection_socket.sendall(message.encode())
+        # print(f'Message lähetetty: {c}')
         data = connection_socket.recv(4096)
         if not data:
             break
--- Networking/echo_server.py
+++ Networking/echo_server.py
@@ -1,24 +1,45 @@
 import socket
+from threading import Thread
 
 HOST = '127.0.0.1'      # Localhost
                         # None / '' / '0.0.0.0' = All interfaces
 PORT = 1234
+stop_signal = False
+
+
+def handle_connection(connection_socket):
+    global stop_signal
+    with connection_socket:
+        while not stop_signal:
+            try:
+                data = connection_socket.recv(4096)
+            except socket.timeout:
+                continue
+            if not data:
+                break
+            message = data.decode()
+            print(f'Received: {message}')
+            if message == 'exit':
+                print('Exit signal received.')
+                stop_signal = True
+                break
+            connection_socket.sendall(data)
+    print('Terminating thread.')
+
+
+socket.setdefaulttimeout(0.3)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listening_socket:
     listening_socket.bind((HOST, PORT))
     listening_socket.listen(1)
 
-    message = None
-    while message != 'exit':
+    while not stop_signal:
         print(f'Waiting for connections on {HOST}:{PORT}')
-        connection_socket, addr = listening_socket.accept()
+        try:
+            connection_socket, addr = listening_socket.accept()
+        except socket.timeout:
+            continue
         print(f'Accepted connection from {addr[0]}:{addr[1]}')
-        with connection_socket:
-            while data := connection_socket.recv(4096):
-                message = data.decode()
-                print(f'Received: {message}')
-                if message == 'exit':
-                    break
-                connection_socket.sendall(data)
-        # if message == 'exit':
-        #     break
+        Thread(target=handle_connection, args=(connection_socket,)).start()
+
+print('Exiting main thread')
